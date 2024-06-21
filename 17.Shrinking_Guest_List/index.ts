f72fafa84f639423ec79d2b37ccb098e3c91d308
MAIRAJ KHAN <160355817+Mairajkhan349@users.noreply.github.com>
2024-06-21 17:36:41 +0500

    first commit

---
+++ 17.Shrinking_Guest_List/index.ts
@@ -0,0 +1,58 @@
+// Assignment No :- 17
+// Question :- Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
+// • Start with your program from Exercise 16. Add a new line that prints a message saying that you can invite only two people for dinner.
+// • Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+// • Print a message to each of the two people still on your list, letting them know they’re still invited.
+// • Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+
+//**The following code is imported from exercise 16 */
+let names_Of_Guests: string[] = [
+    "Riaz Ahmed",
+    "Dr.Aftab Ahmed",
+    "Irfan Ali",
+    "Abdul Hameed Soomro",
+  ];
+  let absent_Guest: string = "Dr.Aftab Ahmed";
+  let new_Guest: string = "Pervaiz Ahmed";
+  names_Of_Guests[1] = new_Guest;
+
+//   for (let i = 0; i < names_Of_Guests.length; i++) {
+//     console.log(
+//       "\n\nDear Mr." +
+//         names_Of_Guests[i] +
+//         "\nIt is our pleasure to have you in the party\nThank You!"
+//     );
+//   }
+
+  console.log("\nGood news! We find a bigger Table so we are inviting 3 more guest.\n");
+
+names_Of_Guests.unshift("Meer Murtaza");
+names_Of_Guests.splice(2 , 0 ,"Ashiq Habib Palijo");
+names_Of_Guests.push("Tahir Ali Mahar");
+
+for (let i = 0; i < names_Of_Guests.length; i++) {
+    console.log(
+      "\n\nDear Mr." +
+        names_Of_Guests[i] +
+        "\nIt is our pleasure to have you in the party\nThank You!"
+    );
+  };
+//** The above code is imported from exercise 16 */
+//** The code for exercise 17 begins from here */ 
+console.log("\nsorry we can not arrange big table,Only two people will be invited\n");
+while (names_Of_Guests.length > 2) {
+   let removed_Guest = names_Of_Guests.pop();
+   console.log(`Sorry Mr. ${removed_Guest},You are not invited for dinner.`);
+   
+}
+for (let i = 0; i < names_Of_Guests.length; i++) {
+    console.log(
+      "\n\nDear Mr." +
+        names_Of_Guests[i] +
+        "\nYou are still invited in the party\nThank You!"
+    );
+  };
+  names_Of_Guests.splice(0,2);
+  console.log(names_Of_Guests);     //**the array is noy empty.*/
+
+  
---
+++ 18.Seeing_The_World/main.ts
@@ -0,0 +1,32 @@
+//Assignment No:- 18
+// Seeing the World: Think of at least five places in the world you’d like to visit.
+
+// • Store the locations in a array. Make sure the array is not in alphabetical order.
+let placesToVisit:string[] = ["Switzerland", "Turkey", "London", "New York", "Paris"];
+// • Print your array in its original order.
+console.log("Original Order:", placesToVisit);
+// • Print your array in alphabetical order without modifying the actual list.
+let alphabeticalOrder = [...placesToVisit] .sort();
+console.log("Alphabetical Order:",alphabeticalOrder);
+// • Show that your array is still in its original order by printing it.
+console.log("Original Order:",placesToVisit);
+// • Print your array in reverse alphabetical order without changing the order of the original list.
+let reverseAlphabeticalOrder = [...placesToVisit].sort().reverse();
+console.log("Reverse Alphabetical Order:", reverseAlphabeticalOrder);
+// • Show that your array is still in its original order by printing it again.
+console.log("Original Order:",placesToVisit);
+// • Reverse the order of your list. Print the array to show that its order has changed.
+placesToVisit.reverse();
+console.log("Reversed Order:", placesToVisit);
+// • Reverse the order of your list again. Print the list to show it’s back to its original order.
+placesToVisit.reverse();
+console.log("Original Order:", placesToVisit); 
+// • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+placesToVisit.sort();
+console.log("Alphabetical Order:", placesToVisit);
+// • Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+placesToVisit.sort().reverse();
+console.log("Reverse Alphabetical Order:", placesToVisit);
+
+
+
